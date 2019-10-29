0e45fb5f555322227714e1fd9276ba7ebaee0e66
Eric Lam <ericlamyauping@gmail.com>
2019-10-30 00:53:05 +1000

    Add a test parties component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PartiesComponent } from './parties/parties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartiesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/parties/parties.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parties',
+  templateUrl: './parties.component.html',
+  styleUrls: ['./parties.component.sass']
+})
+export class PartiesComponent implements OnInit {
+
+  id = 'Girls net party~~';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
